8a17857467b2aca4aa269cd4db497d33a17fc793
BhansaliShreya <Shreya.Bhansali@coforge.com>
2023-12-14 10:28:47 +0530

    Resolved review comments for file download related keywords

--- Keywords/fileDownloadUtils/fileDownload.groovy
+++ Keywords/fileDownloadUtils/fileDownload.groovy
@@ -5,7 +5,6 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-
 import com.kms.katalon.core.annotation.Keyword
 import com.kms.katalon.core.checkpoint.Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
@@ -29,58 +28,49 @@ import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
-
+import com.kms.katalon.core.configuration.RunConfiguration;
 import internal.GlobalVariable
 
 public class fileDownload {
-	
+
 	/**
-	 * Check if file is downloaded.
+	 * Verify if file exists at download path 
 	 *
-	 * @param hyperlinkObject String type object for link
-	 * @param  downloadPath String type object for download path
+	 * @param hyperlinkObject - link clicked to download file from web page
+	 * @param downloadPath - specify the location of downloaded file
 	 */
 	@Keyword
-	public static void fileExist(String hyperlinkObject,String downloadPath) {
+	public static void verifyFileExist(String hyperlinkObject,String downloadPath) {
 		try {
-
 			//Code to get the expected file name from URL
 			String urlAttrValue=WebUI.getAttribute(findTestObject(hyperlinkObject), "href");
 			URL url = new URL(urlAttrValue);
 			String expectedDownloadedFileName = FilenameUtils.getName(url.getPath());
-			KeywordUtil.logInfo("Name of file downloaded :: "+expectedDownloadedFileName);
 
 			//Code to check if file exists at download location
 			String filePath=downloadPath+"/"+expectedDownloadedFileName;
 			File file=new File(filePath);
-
-			if( file.exists())
+			if(file.exists())
 			{
-				KeywordUtil.markPassed("File exists at location ::  "+downloadPath);
-
+				KeywordUtil.markPassed("Name of the file to be downloaded :: "+expectedDownloadedFileName +" Downloaded file exists at location ::  "+filePath);
 			}
 			else {
-				KeywordUtil.markFailed("File doesn't exist.");
+				KeywordUtil.markFailed("File does not exist at location ::  "+filePath);
 			}
-
-		} catch (Exception e) {
+		}catch (Exception e) {
 			e.printStackTrace();
 			KeywordUtil.markFailedAndStop("Exception while finding a file");
 		}
-
-
-
 	}
 
 	/**
-	 * Move the file to other folder
+	 * Move the file to project reports directory
 	 *
-	 * @param hyperlinkObject String type object for link
-	 * @param  sourceFolder String type object for source folder path
+	 * @param hyperlinkObject - link clicked to download file from web page
+	 * @param sourceFolder - source location of the file 
 	 */
 	@Keyword
-	public static void moveFileToProjectDirFolder(String hyperlinkObject,String sourceFolder) {
-
+	public static void moveFileToProjectReportsDirectory(String hyperlinkObject,String sourceFolder) {
 		try {
 			//Code to get the expected file name from URL
 			String urlAttrValue=WebUI.getAttribute(findTestObject(hyperlinkObject), "href");
@@ -97,18 +87,7 @@ public class fileDownload {
 			Path source=Paths.get(sourceFolder+"/"+expectedDownloadedFileName);
 			Path destination=Paths.get(destinationFolder+"/"+expectedDownloadedFileName);
 			Files.move(source,destination);
-			File file=new File(destinationFolder+"/"+expectedDownloadedFileName);
-
-			//Code to verify if file exists on other location after moving
-			if( file.exists())
-			{
-				KeywordUtil.markPassed("File moved succesfully");
-
-			}
-			else {
-				KeywordUtil.markFailed("File move failed");
-			}
-
+			KeywordUtil.logInfo("File is moved succesfully to project Reports directory");
 		}catch (Exception e) {
 			e.printStackTrace();
 			KeywordUtil.markFailedAndStop("Exception while moving a file");
