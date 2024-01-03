cbcc32ae3be5b0ec5907197d46525a79583ea05c
Smit.Agarwal <smit.agarwal@coforge.com>
2024-01-03 19:32:08 +0530

    Code refactoring

--- Keywords/earningsUtils/Dates.groovy
+++ Keywords/earningsUtils/Dates.groovy
@@ -9,15 +9,15 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 public class Dates {
 	/**
-	 * In WebTable column verify if dates are getting displayed in ASC or DESC order
-	 * @param tableObject - Test Object for WebTable referred by OR
-	 * @param columnName - Column Name in which Dates are present
+	 * Verify sorting order of WebTable column with dates i.e. Ascending or Descending
+	 * @param tableObject - test Object for WebTable referred from OR
+	 * @param columnName - column Name in which Dates are present
+	 * @param sortingOrder - sorting order i.e. Ascending or Descending
 	 */
-	@Keyword(keywordObject = "Dates")
-	public static void verifySortingOfDatesColumn(TestObject tableObject, String columnName, String sortingOrder) throws Exception {
+	@Keyword(keywordObject = "WebTable")
+	public static void verifySortingOfDatesColumn(TestObject tableObject, String columnName, String sortingOrder) {
 		try {
 			WebElement tableElement = WebUI.findWebElement(tableObject, 2);
-
 			// Find the index of the target column
 			int columnIndex = -1;
 			List<WebElement> headers = tableElement.findElements(By.tagName("th"));
@@ -28,7 +28,7 @@ public class Dates {
 				}
 			}
 			if (columnIndex == -1) {
-				KeywordUtil.markFailed("Exception in verifySortingOfDatesColumn. Column not found:: " + columnName);
+				KeywordUtil.markFailedAndStop("Searched column not found:: " + columnName);
 			}
 			List<Date> dates = new ArrayList<>();
 			List<WebElement> rows = tableElement.findElements(By.tagName("tr"));
@@ -47,37 +47,39 @@ public class Dates {
 			}
 			// Verify sorting order
 			verifySortingOrderOfDates(dates, sortingOrder);
-		} catch (NoSuchElementException e) {
-			// Ignore "Element not found" exception and give Column name not present exception when user enters incorrect column name
 		} catch (Exception e) {
-			KeywordUtil.logInfo("Exception in verifySortingOfDatesColumn. Error fetching column elements:: " + e.getMessage());
-			throw e;
+			WebUI.comment("Exception :: " + e.getMessage())
+			KeywordUtil.markFailedAndStop("Exception while verifying sorting order of WebTable column with dates");
 		}
 	}
 
 	public static void verifySortingOrderOfDates(List<Date> dates, String sortingOrder) {
 		boolean datesInCorrectOrder = true;
-		for (int i = 1; i < dates.size(); i++) {
-			if (sortingOrder.equalsIgnoreCase("ascending")) {
-				if (dates.get(i).before(dates.get(i - 1))) {
-					datesInCorrectOrder = false;
-					break;
+		try {
+			for (int i = 0; i < dates.size()-1; i++) {
+				if (sortingOrder.equalsIgnoreCase("ascending")) {
+					if (!(dates.get(i).before(dates.get(i + 1)) || dates.get(i).equals(dates.get(i + 1)))) {
+						datesInCorrectOrder = false;
+						break;
+					}
+				} else if (sortingOrder.equalsIgnoreCase("descending")) {
+					if (!(dates.get(i).after(dates.get(i + 1)) || dates.get(i).equals(dates.get(i + 1)))) {
+						datesInCorrectOrder = false;
+						break;
+					}
+				} else {
+					KeywordUtil.markFailedAndStop("Invalid sorting order specified:: " + sortingOrder);
 				}
-			} else if (sortingOrder.equalsIgnoreCase("descending")) {
-				if (dates.get(i).after(dates.get(i - 1))) {
-					datesInCorrectOrder = false;
-					break;
-				}
-			} else {
-				KeywordUtil.markFailed("Exception in verifySortingOfDatesColumn. Invalid sorting order specified:: " + sortingOrder);
-				return;
 			}
-		}
 
-		if (datesInCorrectOrder) {
-			KeywordUtil.logInfo("Dates are in " + sortingOrder + " order");
-		} else {
-			KeywordUtil.markFailedAndStop("Exception in verifySortingOrderOfDates. Dates are not in " + sortingOrder + " order");
+			if (datesInCorrectOrder) {
+				KeywordUtil.logInfo("Dates are in " + sortingOrder + " order");
+			} else {
+				KeywordUtil.markFailedAndStop("Dates are not in " + sortingOrder + " order");
+			}
+		} catch (Exception e) {
+			WebUI.comment("Exception :: " + e.getMessage())
+			KeywordUtil.markFailedAndStop("Exception while verifying sorting order of WebTable column with dates");
 		}
 	}
 }
